64759301c96931a39cf67b859759e85c029ed7dd
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-03-27 23:42:10 -0500

    Adds React front end template with navbar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+
+// Components
+import Navigation from './Navigation';
+
+function App() {
+	return(
+		<Container>
+			<Navigation />
+		</Container>
+	)
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../logo.png';
+
+const Navigation = () => {
+	return(
+			<Navbar>
+				<img 
+					alt="logo" 
+					src={logo} 
+					width="40" 
+					height="40"
+					className="d-inline-block align-top mx-3"
+				/>
+				<Navbar.Brand href="#">Scott Token ICO Crowdsale</Navbar.Brand>
+			</Navbar>
+	)
+}
+
+export default Navigation;
