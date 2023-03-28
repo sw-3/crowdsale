9a907824e7734f53048bcdcf607409b71dd1ffa9
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-03-28 17:55:02 -0500

    connect site to blockchain; add account

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
-import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react'
+
+import { Container } from 'react-bootstrap'
+import { ethers } from 'ethers'
 
 // Components
-import Navigation from './Navigation';
+import Navigation from './Navigation'
+import Info from './Info'
 
 function App() {
+
+	const [account, setAccount] = useState(null)	// defines state var + function to set it
+
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+		const account = ethers.utils.getAddress(accounts[0])
+		setAccount(account)
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	})
+
 	return(
 		<Container>
 			<Navigation />
+			<hr />
+			{account && (
+				<Info account={account} />
+			)}
 		</Container>
 	)
 }
 
-export default App;
+export default App
