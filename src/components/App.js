926f41e50114a2e0982bbaec7dc590284312ce5b
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-03-28 20:49:43 -0500

    get token contract using ABI

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,37 @@ import { ethers } from 'ethers'
 import Navigation from './Navigation'
 import Info from './Info'
 
+// ABIs
+import TOKEN_ABI from '../abis/Token.json'
+import CROWDSALE_ABI from '../abis/Crowdsale.json'
+
 function App() {
 
 	const [account, setAccount] = useState(null)	// defines state var + function to set it
+	const [provider, setProvider] = useState(null)
+
+	const [isLoading, setIsLoading] = useState(true)
 
 	const loadBlockchainData = async () => {
-		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		const provider = new ethers.providers.Web3Provider(window.ethereum)   // this gets metamask interface to BC
+		setProvider(provider)
+
+		// initiate contracts
+		const token = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', TOKEN_ABI, provider)
+		console.log(token)
 
 		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
 		const account = ethers.utils.getAddress(accounts[0])
 		setAccount(account)
+
+		setIsLoading(false)
 	}
 
 	useEffect(() => {
-		loadBlockchainData()
-	})
+		if (isLoading) {
+			loadBlockchainData()
+		}
+	}, [isLoading])
 
 	return(
 		<Container>
