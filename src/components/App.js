d6338db86ec08449250c8940f37dbffd87130e06
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-03-28 22:39:00 -0500

    displays crowdsale info on page

--- src/components/App.js
+++ src/components/App.js
@@ -6,29 +6,55 @@ import { ethers } from 'ethers'
 // Components
 import Navigation from './Navigation'
 import Info from './Info'
+import Loading from './Loading'
+import Progress from './Progress'
 
 // ABIs
 import TOKEN_ABI from '../abis/Token.json'
 import CROWDSALE_ABI from '../abis/Crowdsale.json'
 
-function App() {
+// config
+import config from '../config.json'
 
-	const [account, setAccount] = useState(null)	// defines state var + function to set it
+function App() {
 	const [provider, setProvider] = useState(null)
+	const [crowdsale, setCrowdsale] = useState(null)
+
+	const [account, setAccount] = useState(null)
+	const [accountBalance, setAccountBalance] = useState(0)
+
+	const [price, setPrice] = useState(0)
+	const [maxTokens, setMaxTokens] = useState(0)
+	const [tokensSold, setTokensSold] = useState(0)
 
 	const [isLoading, setIsLoading] = useState(true)
 
 	const loadBlockchainData = async () => {
+		// initiate provider
 		const provider = new ethers.providers.Web3Provider(window.ethereum)   // this gets metamask interface to BC
 		setProvider(provider)
 
 		// initiate contracts
-		const token = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', TOKEN_ABI, provider)
-		console.log(token)
+		const token = new ethers.Contract(config[31337].token.address, TOKEN_ABI, provider)
+		const crowdsale = new ethers.Contract(config[31337].crowdsale.address, CROWDSALE_ABI, provider)
+		setCrowdsale(crowdsale)
 
+		// fetch accounts
 		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
 		const account = ethers.utils.getAddress(accounts[0])
 		setAccount(account)
+
+		// fetch account balance
+		const accountBalance = ethers.utils.formatUnits(await token.balanceOf(account), 18)
+		setAccountBalance(accountBalance)
+
+		// fetch crowdsale data
+		const price = ethers.utils.formatUnits(await crowdsale.price(), 18)
+		setPrice(price)
+		const maxTokens = ethers.utils.formatUnits(await crowdsale.maxTokens(), 18)
+		setMaxTokens(maxTokens)
+		const tokensSold = ethers.utils.formatUnits(await crowdsale.tokensSold(), 18)
+		setTokensSold(tokensSold)
 
 		setIsLoading(false)
 	}
@@ -42,9 +68,22 @@ function App() {
 	return(
 		<Container>
 			<Navigation />
+
+			<h1 className='my-4 text-center'>Introducing Scott Token!</h1>
+
+			{isLoading ? (
+				<Loading />
+			) : (
+				<>
+					<p className='text-center'><strong>Current Price:</strong> {price} ETH</p>
+					<Progress maxTokens={maxTokens} tokensSold={tokensSold}/>
+				</>
+			)}
+
 			<hr />
+
 			{account && (
-				<Info account={account} />
+				<Info account={account} accountBalance={accountBalance} />
 			)}
 		</Container>
 	)
