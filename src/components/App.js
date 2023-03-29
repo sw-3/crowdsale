bd5641c2685cc72a996a3be88afdc7a865676aff
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-03-28 22:50:18 -0500

    fixed chainId to not be hard-coded

--- src/components/App.js
+++ src/components/App.js
@@ -33,10 +33,11 @@ function App() {
 		// initiate provider
 		const provider = new ethers.providers.Web3Provider(window.ethereum)   // this gets metamask interface to BC
 		setProvider(provider)
+		const { chainId } = await provider.getNetwork()
 
 		// initiate contracts
-		const token = new ethers.Contract(config[31337].token.address, TOKEN_ABI, provider)
-		const crowdsale = new ethers.Contract(config[31337].crowdsale.address, CROWDSALE_ABI, provider)
+		const token = new ethers.Contract(config[chainId].token.address, TOKEN_ABI, provider)
+		const crowdsale = new ethers.Contract(config[chainId].crowdsale.address, CROWDSALE_ABI, provider)
 		setCrowdsale(crowdsale)
 
 		// fetch accounts
