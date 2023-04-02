a0cce4c3575af03f6dbb7aa8927889f3a37b6d67
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-04-01 23:52:20 -0500

    creates deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+// Modified by commenting out the SCOT token deploy section.
+// Instead, will use the already-deployed SW3T token on Mumbai.
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Scott Token'
-  const SYMBOL = 'SCOT'
-  const MAX_SUPPLY = '100000000'
+//  const NAME = 'Scott Token'
+//  const SYMBOL = 'SCOT'
+  const MAX_SUPPLY = '1000000'
   const PRICE = ethers.utils.parseUnits('0.000025', 'ether')
 
-  // Deploy token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  // get token contract for SW3T
+  const tokenAddr = '0xf5D9957F0DBbedaAEA9cadF99aD2b7965Fcc910A'
+  const Token = await ethers.getContractFactory('Token')
+  const token = Token.attach(tokenAddr)
+
+//  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+//  await token.deployed()
+//  console.log(`Token deployed to: ${token.address}\n`)
+  console.log(`Attached to Token deployed at: ${token.address}\n`)
 
   // Deploy Crowdsale
-  const Crowdsale = await hre.ethers.getContractFactory('Crowdsale')
-  const crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  const Crowdsale = await ethers.getContractFactory('Crowdsale')
+  const crowdsale = await Crowdsale.deploy(tokenAddr, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
   await crowdsale.deployed()
   console.log(`Crowdsale deployed to: ${crowdsale.address}\n`)
 
