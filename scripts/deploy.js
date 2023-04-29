6b20de91c5ac34cc729b9bf37bb1e97243c8e92c
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-04-28 22:42:12 -0500

    updates deployments config and abi

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,36 +1,48 @@
-// Modified by commenting out the SCOT token deploy section.
-// Instead, will use the already-deployed SW3T token on Mumbai.
+// Deploy SW3 Token and the Crowdsale contract.
+// NOTE the commented section, if you want to deploy Crowdsale for a token that
+//      has already been deployed.
+
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Scott Token'
-  const SYMBOL = 'SCOT'
-  const MAX_SUPPLY = '1000000'
-  const PRICE = ethers.utils.parseUnits('0.000025', 'ether')
+  const NAME = 'SW3 Token'
+  const SYMBOL = 'SW3'
+  const TOKEN_SUPPLY = '1000000'
+  const CROWDSALE_SUPPLY = '400000'   // selling 40% of token supply
+  const PRICE = ethers.utils.parseUnits('0.0005', 'ether')
 
   const Token = await ethers.getContractFactory('Token')
 
-//  Uncomment this block to get token contract for existing SW3T on Mumbai testnet
+//  Uncomment this block to get existing token contract on the network
 //  ------------------------------------------------------------------------------
 //  NOTE: use tokenAddr in Crowdsale.deploy call below, instead of token.address
 //
-//  const tokenAddr = '0xf5D9957F0DBbedaAEA9cadF99aD2b7965Fcc910A' 
+//  const tokenAddr = '0x...'
 //  const token = Token.attach(tokenAddr)
 //  console.log(`Attached to Token deployed at: ${token.address}\n`)
 
-  // Comment this block, if deploying with existing token in previous block
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  // Comment out this block, if deploying with existing token in previous block
+  let token = await Token.deploy(NAME, SYMBOL, TOKEN_SUPPLY)
   await token.deployed()
   console.log(`Token deployed to: ${token.address}\n`)
 
   // Deploy Crowdsale
   const Crowdsale = await ethers.getContractFactory('Crowdsale')
-  const crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  const crowdsale = await Crowdsale.deploy
+                            (
+                              token.address,
+                              PRICE,
+                              ethers.utils.parseUnits(CROWDSALE_SUPPLY, 'ether')
+                            )
   await crowdsale.deployed()
   console.log(`Crowdsale deployed to: ${crowdsale.address}\n`)
 
   // send tokens to crowdsale
-  const transaction = await token.transfer(crowdsale.address, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  const transaction = await token.transfer
+                              (
+                                crowdsale.address,
+                                ethers.utils.parseUnits(CROWDSALE_SUPPLY, 'ether')
+                              )
   await transaction.wait()
 
   console.log(`Tokens transferred to Crowdsale\n`)
